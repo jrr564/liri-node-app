1d02379b1862bb58fd432fc703c3465691794132
admin <admin@vraney-mbp.thelamplighterschool.org>
2018-01-16 16:11:47 -0600

    twitter works

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.ezaC5Y8qh59LO5QRYyqlanESW,
-  consumer_secret: process.env.CaI5atXPKUlavy9moXk37kouTYvyEREw5aThnYg5JdMcI0JtVU,
-  access_token_key: process.env.951847481347858434-bfXKD8b1EM3r9eSbhflrZKWd8zRhz4a,
-  access_token_secret: process.env.BjeIdA54Z6xW2TFHUjCV61rQ82aQDyOBnHK32pfVbQDcT
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
--- liri.js
+++ liri.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
-var Twitter = require('twitter');
-var spotify = new Spotify(keys.spotify);
+var keys = require('./keys.js');
+var twitter = require('twitter');
+var request = require('request');
+var spotify = require('spotify');
+
+var spotify = new spotify(keys.spotify);
+var client = new twitter(keys.twitter);
 //twitter node
-var client = new Twitter({
+var client = new twitter({
   consumer_key: '',
   consumer_secret: '',
   access_token_key: '',
@@ -11,29 +16,77 @@ var client = new Twitter({
 });
 
 //takes in node command
-var request = process.argv[2];
-var request = process.argv[3];
+var input = process.argv;
+var request = input[2];
+var selection = input[3];
+
+var getTweets = function() {
+  var client = new twitter(keys.twitter);
+
+  var params = {screen_name: 'RealUSADude', count: 20 };
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+    if (!error) {
+      var data = []; //empty array to hold data
+      for (var i = 0; i < tweets.length; i++) {
+        data.push({
+	       'Tweets: ' : tweets[i].text,
+	       'created at: ' : tweets[i].created_at,
+        });
+      }
+      console.log(data);
+    }
+  });
+};
+
+getTweets();
+		
+
+// T.get('search/tweets', params, function(err, data, response) {
+//   if(!err){
+//     // This is where the magic will happen
+//   } else {
+//     console.log(T);
+//     console.log(params);
+//     console.log(Twitter)
+//   }
+// });
+
+// for (let i = 0 <data.statuses.length, i++) {
+// 	let id = {id = data.statuses[i].id-str}
+// }
+
+
+// var T = new Twitter(config);
+// var params = {
+// 	q: '#nodejs',
+// 	count: 10,
+// 	result_type: 'recent',
+// 	lang: 'en'
+// }
+
+// T.get('search/tweets', params, function(err, data, response) {
+//   if(!err){
+//   } else {
+//     console.log(err);
+//   }
+// })
+// //function 
+
+// // } else if (request === "spotify-this-song") {
 
 
 
-
-if (request === "my-tweets") {
-
-
-
-} else if (request === "spotify-this-song") {
+// // } else if (request === "movie-this") {
 
 
 
-} else if (request === "movie-this") {
+// // } else if (request === "do-what-it-says") {
 
 
-
-} else if (request === "do-what-it-says") {
-
-
-} else {
-	console.log("Please make another request.")
-}
+// // } else {
+// // 	console.log("Please make another request, dog")
+// // }
 
 

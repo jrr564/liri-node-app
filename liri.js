3ad413b4e81b007e768a4d0bf82eb4635b918f12
admin <admin@vraney-mbp.thelamplighterschool.org>
2018-01-17 15:48:59 -0600

    closer

--- liri.js
+++ liri.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
-
+//Load Node Modules
 var keys = require('./keys.js');
 var twitter = require('twitter');
 var request = require('request');
-var spotify = require('spotify');
+var spotify = require('node-spotify-api');
 
-var spotify = new spotify(keys.spotify);
+//takes in node command
+var input = process.argv;
+var command = input[2];
+var selection = input[3];
+
+//loads Twitter and Spotify Keys
 var client = new twitter(keys.twitter);
+var spotifyAPI = new spotify(keys.spotify);
+
 //twitter node
 var client = new twitter({
   consumer_key: '',
@@ -14,79 +21,59 @@ var client = new twitter({
   access_token_key: '',
   access_token_secret: ''
 });
-
-//takes in node command
-var input = process.argv;
-var request = input[2];
-var selection = input[3];
-
+//Send request to Twitter API
 var getTweets = function() {
-  var client = new twitter(keys.twitter);
-
-  var params = {screen_name: 'RealUSADude', count: 20 };
-
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-
-    if (!error) {
-      var data = []; //empty array to hold data
-      for (var i = 0; i < tweets.length; i++) {
-        data.push({
-	       'Tweets: ' : tweets[i].text,
-	       'created at: ' : tweets[i].created_at,
-        });
-      }
-      console.log(data);
+	var client = new twitter(keys.twitter);
+	var params = {screen_name: selection, count: 20 };
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+		var data = []; //empty array to hold data
+		for (var i = 0; i < tweets.length; i++) {
+		data.push({
+			"Tweet: " : tweets[i].text,
+			"Timestamp: " : tweets[i].created_at,
+		});
+		}
+		console.log(data);
     }
   });
 };
+//Send request to Spotify API
+function spotifyThis() {
+	spotifyAPI.search({ type: 'track', query: selection}, function(err, data) {
+		if ( err ) {
+		console.log('Error occurred: ' + err);
+		return;  //from spotify npm docs
+	} else{
+		var songInfo = data.tracks.items[0];
+		var songResult = console.log(songInfo.artists[0].name)
+			console.log("--------------------------------")
+			console.log(songInfo.name)
+			console.log(songInfo.preview_url)
+			console.log(songInfo.album.name)
+			console.log(songResult);
+			console.log("--------------------------------")
+    };
+  });
+}  
+//using request to grab JSON object of OMDB API
+function omdbAPI() {
+	request("http://www.omdbapi.com/?t=" + selection +"=&plot=&tomatoes=true=short&apikey=trilogy",
+		function(error, response, body) {
+	  if (!error && response.statusCode === 200) {
+	  	console.log("--------------------------------");
+		console.log("TITLE: " + JSON.parse(body).Title);
+		console.log("RELEASED: " + JSON.parse(body).Released);
+		console.log("IMDB RATING: " + JSON.parse(body).imdbRating);
+		console.log("ROTTEN TOMATOES RATING: " + JSON.parse(body).Ratings[1].Value);
+		console.log("LANGUAGE: " + JSON.parse(body).Language);
+		console.log("PLOT: " + JSON.parse(body).Plot);
+		console.log("CAST: " + JSON.parse(body).Actors);
+		console.log("--------------------------------")
+		}
+	})
+};
 
 getTweets();
-		
-
-// T.get('search/tweets', params, function(err, data, response) {
-//   if(!err){
-//     // This is where the magic will happen
-//   } else {
-//     console.log(T);
-//     console.log(params);
-//     console.log(Twitter)
-//   }
-// });
-
-// for (let i = 0 <data.statuses.length, i++) {
-// 	let id = {id = data.statuses[i].id-str}
-// }
-
-
-// var T = new Twitter(config);
-// var params = {
-// 	q: '#nodejs',
-// 	count: 10,
-// 	result_type: 'recent',
-// 	lang: 'en'
-// }
-
-// T.get('search/tweets', params, function(err, data, response) {
-//   if(!err){
-//   } else {
-//     console.log(err);
-//   }
-// })
-// //function 
-
-// // } else if (request === "spotify-this-song") {
-
-
-
-// // } else if (request === "movie-this") {
-
-
-
-// // } else if (request === "do-what-it-says") {
-
-
-// // } else {
-// // 	console.log("Please make another request, dog")
-// // }
-
-
+spotifyThis();
+omdbAPI();
